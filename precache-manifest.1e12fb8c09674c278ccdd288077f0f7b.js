eb73e988f223b6aeb96b14caa2e5fa9f61265d22
Grey <grey@Mac.localdomain>
2025-03-01 13:21:32 -0800

    Updates

---
+++ precache-manifest.1e12fb8c09674c278ccdd288077f0f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29655bdc42c14b0660956971a45b6f0f",
+    "url": "/workout-tracker/index.html"
+  },
+  {
+    "revision": "8248bfaa7543b16dfebb",
+    "url": "/workout-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/workout-tracker/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/workout-tracker/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "8248bfaa7543b16dfebb",
+    "url": "/workout-tracker/static/js/main.b389136a.chunk.js"
+  },
+  {
+    "revision": "4fab0fe3a108275851e5",
+    "url": "/workout-tracker/static/js/runtime~main.de2a187d.js"
+  }
+]);
